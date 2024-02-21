55d85a89ea1f7824fb6e625cd0972df375cf6fff
Nickurama <rokdoombeast@gmail.com>
2024-02-21 04:24:11 +0000

    Improved areEqual

--- Lab2/D/MathUtil.java
+++ Lab2/D/MathUtil.java
@@ -1,9 +1,9 @@
 public class MathUtil
 {
-    private static final double CALC_ERROR_MARGIN = 0.0000000005;
+    private static final double CALC_ERROR_MARGIN = 0.000000001;
 
     public static boolean areEqual(double a, double b)
     {
-        return b < a + CALC_ERROR_MARGIN && b > a - CALC_ERROR_MARGIN;
+        return Math.abs(b - a) < CALC_ERROR_MARGIN;
     }
 }
